33016d43efc23a525ce5478eb92357fac44ff122
Nima4180 <146990855+Nima4180@users.noreply.github.com>
2025-01-05 14:06:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is where you can add JavaScript for interactivity in the future.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to TreadMtl! The website is loaded.");
+});
